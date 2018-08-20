89296728f2f1a72d2e1f15f7b0a74c1cc9a63d37
K-9999 <huangshuren1990@gmail.com>
2018-08-20 21:24:13 +0800

    设置PathVariable，添加url中的变量

--- src/main/java/com/leaf/Application.java
+++ src/main/java/com/leaf/Application.java
@@ -2,6 +2,7 @@ package com.leaf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,16 @@ public class Application {
     @RequestMapping("/methodPath")
     public String method(){
         return "mapping url is /classPath/methodPath";
+    }
+
+    @RequestMapping("/users/{username}")
+    public String userProfile(@PathVariable("username") String username){
+        return String.format("user %s", username);
+    }
+
+    @RequestMapping("/posts/{id}")
+    public String post(@PathVariable("id") int id){
+        return String.format("post %d", id);
     }
 
     public static void main(String[] args) {
