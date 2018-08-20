25cabaa490d3c3176c7546948e4d7e3e832e5256
K-9999 <huangshuren1990@gmail.com>
2018-08-20 22:43:16 +0800

    添加模板渲染

--- src/main/java/com/leaf/Application.java
+++ src/main/java/com/leaf/Application.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author huangshuren
+ */
 @SpringBootApplication
 @RestController
 @RequestMapping("/classPath")
---
+++ src/main/java/com/leaf/HelloController.java
@@ -0,0 +1,19 @@
+package com.leaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author huangshuren
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
