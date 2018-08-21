2ead41d3ae67e487c7e64642c48c92441d15353a
K-9999 <huangshuren1990@gmail.com>
2018-08-21 16:07:28 +0800

    注解@PostMapping只能在高版本的Spring中使用，1.2.5版本使用不了

--- src/main/java/com/leaf/FileUploadController.java
+++ src/main/java/com/leaf/FileUploadController.java
@@ -1,7 +1,9 @@
 package com.leaf;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,7 +13,7 @@ import java.io.IOException;
 @Controller
 public class FileUploadController {
 
-    @PostMapping("/upload")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ResponseBody
     public String handleFileUpload(@RequestParam("file") MultipartFile file) throws IOException {
         byte[] bytes = file.getBytes();
